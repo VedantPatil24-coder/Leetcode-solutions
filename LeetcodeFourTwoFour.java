9cdf277ce86206be806091ccf9012262d2f67a3d
VedantPatil24-coder <patilvedant299@gmail.com>
2026-06-23 08:43:47 +0000

    Add LeetcodeFourTwoFour solution file

---
+++ LeetcodeFourTwoFour.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+        int left = 0;
+        int right = 0;
+        int maxFreq = 0;
+        int result = 0;
+        while(right<s.length()){
+            freq[s.charAt(right)-'A']++;
+            maxFreq = Math.max(maxFreq, freq[s.charAt(right) - 'A']);
+            if(right-left+1 - maxFreq > k){
+                freq[s.charAt(left)-'A']--;
+                left++;
+            }
+            result = Math.max(result, right-left+1);
+            right++;
+        }
+        return result;
+    }
+}
